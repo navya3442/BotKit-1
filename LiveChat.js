44b81bb001c74c42cc43ff07efbbcd3112807415
navya3442 <navyasri.chirumalla@kore.com>
2025-01-30 10:36:03 +0530

    chathistory

--- LiveChat.js
+++ LiveChat.js
@@ -1,5 +1,5 @@
-var botId               = "st-f82726cb-4cd6-5ec4-bd35-7f5542b616dc";
-var botName             = "AgentTransferDemoBot";
+var botId               = "st-2dfb9345-6f6e-5d7f-a70e-dc2926401e42";
+var botName             = "test1";
 var sdk                 = require("./lib/sdk");
 var Promise             = require('bluebird');
 var api                 = require('./LiveChatAPI.js');
@@ -7,6 +7,8 @@ var _                   = require('lodash');
 var config              = require('./config.json');
 var debug               = require('debug')("Agent");
 var schedular           = require('node-schedule');
+var { makeHttpCall } = require("./makeHttpCall");
+var jwt = require("jwt-simple");
 var _map                = {}; //used to store secure session ids //TODO: need to find clear map var
 var userDataMap         = {};//this will be use to store the data object for each user
 var userResponseDataMap = {};
@@ -163,12 +165,19 @@ function onUserMessage(requestId, data, cb){
     debug("user message", data);
     var visitorId = _.get(data, 'channel.from');
     var entry = _map[visitorId];
+    if(data.message == 'history'){
+        getChathistory(data)
+        data.message="Processing...chat history";
+        return sdk.sendUserMessage(data, cb);
+    }
     if(entry){//check for live agent
         //route to live agent
         var formdata = {};
         formdata.secured_session_id = entry.secured_session_id;
         formdata.licence_id = config.liveagentlicense;
         formdata.message = data.message;
+
+
         return api.sendMsg(visitorId, formdata)
             .catch(function(e){
                 console.error(e);
@@ -196,7 +205,6 @@ module.exports = {
     botId : botId,
     botName : botName,
     on_user_message : function(requestId, data, callback) {
-        console.log('data', data);
         debug('on_user_message');
         onUserMessage(requestId, data, callback);
     },
@@ -208,5 +216,108 @@ module.exports = {
         debug('on_webhook');
         onAgentTransfer(requestId, data, callback);
     },
+    on_event: function(requestId, data, callback) {
+            // fetchAllBotVariables(data);
+            return callback(null, data);
+        },
+    on_alert: function(requestId, data, callback) {
+            // fetchAllBotVariables(data);
+            return sdk.sendAlertMessage(data, callback);
+        },
+        on_variable_update: function(requestId, data, callback) {
+            var event = data.eventType;
+            // if (first || event == "bot_import" || event == "variable_import" || event == "sdk_subscription" || event == "language_enabled") {
+            //     sdk.fetchBotVariable(data, langArr, function(err, response) {
+            //         dataStore.saveAllVariables(response, langArr);
+            //         first = false;
+            //     });
+            // } else {
+            //     var lang = data.language;
+            //     //update Exixting BotVariables in Storage
+            //     updateBotVariableInDataStore(botVariables, data, event, lang);
+            // }
+            // console.log(dataStore);
+    
+        },
     gethistory: gethistory
 };
+function fetchAllBotVariables(data) {
+        sdk.fetchBotVariable(data, langArr, function(err, response) {
+            first = false;
+            dataStore.saveAllVariables(response, langArr);
+        });
+
+}
+
+function updateBotVariableInDataStore(botVariables, data, event, lang) {
+    var variable = data.variable;
+    if (event === "variable_create") {
+        //update storage with newly created variable
+        for (var i = 0; i < langArr.length; i++) {
+            dataStore.addVariable(variable, i);
+        }
+    } else if (event == "variable_update") {
+        //update storage with updated variable
+        var index = langArr.indexOf(lang);
+        if (index > -1) {
+            dataStore.updateVariable(variable, langArr, index);
+        }
+    } else if (event == "variable_delete") {
+        //delete variable from storage
+        dataStore.deleteVariable(variable, langArr);
+    }
+}
+
+function getChathistory(requestData){  
+    if(requestData) {        
+        var limit  = requestData.limit || 20;
+        var offset = requestData.skip || 0;
+        var userId = requestData.channel.handle.userId;
+        var botId = requestData.botId;        
+        var url    = `https://bots.kore.ai/api/public/bot/${botId}/getMessages?&userId=${userId}`;
+        var headers;
+        if (!headers) {
+            headers = {};   
+        }
+        headers['content-type'] = 'application/json';
+        headers.auth = getSignedJWTToken(botId);
+        const body   = {}
+        return makeHttpCall('get',url,body,headers)
+        .then(function(res){    
+		const historyData = res.data.messages.map(obj => ({
+		type: obj.type === "outgoing" ? "bot" : "user",
+		text: obj.components[0]?.data?.text || "No text found"
+		}));
+		console.log("historyData",historyData);
+            return historyData;
+        }).catch(function(err){
+            console.log(err);        
+        }); 
+    }
+}
+
+function getSignedJWTToken(botId) {
+    var appId, apiKey, jwtAlgorithm, jwtExpiry;
+    var defAlg = "HS256";
+
+    if (config.credentials[botId]) {
+        appId = config.credentials[botId].appId;
+        apiKey = config.credentials[botId].apikey;
+    } else {
+        appId = config.credentials.appId;
+        apiKey = config.credentials.apikey;
+    }
+
+    if (config.jwt[botId]) {
+        jwtAlgorithm = config.jwt[botId].jwtAlgorithm;
+        jwtExpiry = config.jwt[botId].jwtExpiry;
+    } else {
+        jwtAlgorithm = config.jwt.jwtAlgorithm;
+        jwtExpiry = config.jwt.jwtExpiry;
+    }
+
+    return jwt.encode({ 
+        appId: appId, 
+        exp: Date.now()/1000 + (jwtExpiry || 60) //set the default expiry as 60 seconds
+    }, apiKey, (jwtAlgorithm || defAlg));
+}
